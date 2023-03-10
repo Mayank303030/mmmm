7570cc292053e571993d687142fdb21fa8a90ca8
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 19:52:06 +0000

    Add files via upload

---
+++ m.js
@@ -0,0 +1,8 @@
+const storyButton = document.querySelector('.story-button');
+storyButton.addEventListener('click', function() {
+    // Code to handle click on story button goes here
+});
+const storyButton = document.querySelector('.story-button');
+storyButton.addEventListener('click', function() {
+    window.open('https://www.instagram.com/stories/create');
+});
